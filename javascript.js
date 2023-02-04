c395d012fdd5225a1c9c6834a76e6e4f5629cb1c
Brad <117415722+bktpp@users.noreply.github.com>
2023-02-03 19:11:28 -0600

    added buttons, code to interact with buttons

--- javascript.js
+++ javascript.js
@@ -1,48 +1,59 @@
-const options = ["ROCK", "PAPER", "SCISSORS"];
  
- function getComputerChoice() {
+ const options = ["ROCK", "PAPER", "SCISSORS"];
+ 
+function getComputerChoice() {
     const choice = options[Math.floor(Math.random() * options.length)];
     return choice;
 } 
+let playerPick = '';
+function playersChoice(choice) {
+playerPick = choice;
+playRound();
+}
+
+document.querySelector('.rock').addEventListener('click', function() {
+playersChoice('ROCK');
+})
+document.querySelector('.paper').addEventListener('click', function() {
+  playersChoice('PAPER');
+  })
+  document.querySelector('.scissors').addEventListener('click', function() {
+    playersChoice('SCISSORS');
+    })
 /* This function chooses a random name from options array to use as
 the computers choice in game */
 
 
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection == computerSelection) {
-        return "Tie";
+ function playRound() {
+    const computerSelection = getComputerChoice();
+    if(playerPick == computerSelection) {
+      console.log('TIE');
+        return 'TIE';
     }
     else if(
-           (playerSelection == "ROCK" && computerSelection == "SCISSORS") ||
-           (playerSelection == "PAPER" && computerSelection == "ROCK") ||
-           (playerSelection == "SCISSORS" && computerSelection == "PAPER"))
+           (playerPick == "ROCK" && computerSelection == "SCISSORS") ||
+           (playerPick == "PAPER" && computerSelection == "ROCK") ||
+           (playerPick == "SCISSORS" && computerSelection == "PAPER"))
    
      {
-    return `Player wins, ${playerSelection} beats ${computerSelection}`;
-} else {
-    return `computer wins, ${computerSelection} beats ${playerSelection}`;
+      console.log(`Player wins, ${playerPick} beats ${computerSelection}`);
+    return `Player wins, ${playerPick} beats ${computerSelection}`;
+  } else {
+    console.log(`computer wins, ${computerSelection} beats ${playerPick}`);
+    return `computer wins, ${computerSelection} beats ${playerPick}`;
  }
 } 
+
+
 /* This function calls players and computers choice and evaluates who won that round */
 
-function getPlayerChoice() {
-  let goodAnswer = false;
-  while(goodAnswer == false) {
-      const playerAnswer = prompt("Rock, paper or scissors?");
-    
-     if(playerAnswer == null){
-          continue;
-  }
-    const playerAnsCaps = playerAnswer.trim().toUpperCase();
-    if(options.includes(playerAnsCaps)) {
-      goodAnswer = true;
-      return playerAnsCaps;
-    }
-} }
+
+ 
+
 
 //function for getting players choice
 
-function game() {
+/*function game() {
   let playerScore = 0;
   let computerScore = 0;
 
@@ -68,7 +79,6 @@ function game() {
         } else {
           console.log("DRAW");
         }
-      }
+      } 
      /* function for a 5 round match and announces winner based on score at the end */
-game();
-
+// game(); 
