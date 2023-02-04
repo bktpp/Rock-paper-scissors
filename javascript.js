9730a2da9a3835ab9476dcae6c3a2d720766c024
Brad <117415722+bktpp@users.noreply.github.com>
2023-02-04 17:45:16 -0600

    finalized code for results showing in dom

--- javascript.js
+++ javascript.js
@@ -1,84 +1,97 @@
+const options = ["ROCK", "PAPER", "SCISSORS"];
  
- const options = ["ROCK", "PAPER", "SCISSORS"];
- 
-function getComputerChoice() {
+ function getComputerChoice() {
     const choice = options[Math.floor(Math.random() * options.length)];
     return choice;
 } 
+
+const gameResult = document.querySelector('.result');
+
 let playerPick = '';
-function playersChoice(choice) {
-playerPick = choice;
-playRound();
+let computerPick = getComputerChoice();
+document.querySelector('.rock').addEventListener('click', playerChoseRock);
+
+function playerChoseRock() {
+      playerPick = 'ROCK';
+      computerPick = getComputerChoice();
+  if (playerPick == computerPick) {
+    rounds++;
+    gameResult.textContent = "";
+    gameResult.textContent = "Tie!";
+  } else if (computerPick == 'PAPER') {
+    rounds++;
+    computerScore++;
+    gameResult.textContent = "";
+    gameResult.textContent = `Computer won! ${computerPick} beats ${playerPick}`;
+  } else {
+    rounds++;
+    playerScore++;
+    gameResult.textContent = "";
+    gameResult.textContent = `player won, ${playerPick} beats ${computerPick}`;
+  }
+  scoreBoard();
 }
 
-document.querySelector('.rock').addEventListener('click', function() {
-playersChoice('ROCK');
-})
-document.querySelector('.paper').addEventListener('click', function() {
-  playersChoice('PAPER');
-  })
-  document.querySelector('.scissors').addEventListener('click', function() {
-    playersChoice('SCISSORS');
-    })
-/* This function chooses a random name from options array to use as
-the computers choice in game */
+document.querySelector('.paper').addEventListener('click', playerChosePaper);
 
-
- function playRound() {
-    const computerSelection = getComputerChoice();
-    if(playerPick == computerSelection) {
-      console.log('TIE');
-        return 'TIE';
-    }
-    else if(
-           (playerPick == "ROCK" && computerSelection == "SCISSORS") ||
-           (playerPick == "PAPER" && computerSelection == "ROCK") ||
-           (playerPick == "SCISSORS" && computerSelection == "PAPER"))
-   
-     {
-      console.log(`Player wins, ${playerPick} beats ${computerSelection}`);
-    return `Player wins, ${playerPick} beats ${computerSelection}`;
-  } else {
-    console.log(`computer wins, ${computerSelection} beats ${playerPick}`);
-    return `computer wins, ${computerSelection} beats ${playerPick}`;
- }
-} 
-
-
-/* This function calls players and computers choice and evaluates who won that round */
-
-
- 
-
-
-//function for getting players choice
-
-/*function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-
-    if(playRound(playerSelection, computerSelection) == `Player wins, ${playerSelection} beats ${computerSelection}`) {
+function playerChosePaper() {
+    playerPick = 'PAPER';
+    computerPick = getComputerChoice();
+    if (playerPick == computerPick) {
+      rounds++;
+      gameResult.textContent = "";
+      gameResult.textContent = `Tie!`;
+    } else if (computerPick == 'SCISSORS') {
+      rounds++;
+      computerScore++;
+      gameResult.textContent = "";
+      gameResult.textContent = `computer won, ${computerPick} beats ${playerPick}`;
+    } else {
+      rounds++;
       playerScore++;
+      gameResult.textContent = "";
+      gameResult.textContent = `player won, ${playerPick} beats ${computerPick}`;
     }
-      else if(playRound(playerSelection, computerSelection) == `computer wins, ${computerSelection} beats ${playerSelection}`) {
-        computerScore++;
-      }
+    scoreBoard();
+}
+
+document.querySelector('.scissors').addEventListener('click', playerChoseScissors);
+
+function playerChoseScissors() {
+    playerPick = 'SCISSORS';
+    computerPick = getComputerChoice();
+    if (playerPick == computerPick) {
+      rounds++;
+      gameResult.textContent = "";
+      gameResult.textContent = `Tie!`;
+    } else if (computerPick == 'ROCK') {
+      rounds++;
+      computerScore++;
+      gameResult.textContent = "";
+      gameResult.textContent = `computer won, ${computerPick} beats ${playerPick}`;
+    } else {
+      rounds++;
+      playerScore++;
+      gameResult.textContent = "";
+      gameResult.textContent = `player won, ${playerPick} beats ${computerPick}`;
     } 
-      console.log("GAME OVER");
-      if(playerScore > computerScore) {
-        console.log("WINNER WINNER CHICKEN DINNER!");
-      }
-        else if(playerScore < computerScore) {
-          console.log("Hold this L");
-        } else {
-          console.log("DRAW");
-        }
-      } 
-     /* function for a 5 round match and announces winner based on score at the end */
-// game(); 
+    scoreBoard();
+}
+let rounds = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+function scoreBoard() {
+  if (rounds == 5) {
+    if (playerScore > computerScore) {
+      gameResult.textContent = "";
+      gameResult.textContent = `Player won!, ${playerScore} to ${computerScore}`;
+      gameResult.style.fontSize = '50px'
+    } else {
+      gameResult.textContent = "";
+      gameResult.textContent = `computer won!, ${computerScore} to ${playerScore}`;
+      gameResult.style.fontSize = '50px'
+    }
+  }
+}
+
